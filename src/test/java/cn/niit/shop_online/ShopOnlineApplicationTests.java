43be19d08ccb0ab1db2d502457fd43be6723567a
sadwihu <2254443557@qq.com>
2023-11-07 22:36:28 +0800

    Initial commit

---
+++ src/test/java/cn/niit/shop_online/ShopOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.niit.shop_online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
