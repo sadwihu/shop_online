171265d1202547d3c7c25ffb58c8f90cca8c52fe
sadwihu <2254443557@qq.com>
2023-11-11 18:28:26 +0800

    商品模块接口

--- src/main/java/com/soft2242/shop/entity/Category.java
+++ src/main/java/com/soft2242/shop/entity/Category.java
@@ -1,23 +1,20 @@
 package com.soft2242.shop.entity;
 
-import com.baomidou.mybatisplus.annotation.FieldFill;
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableLogic;
-import com.baomidou.mybatisplus.annotation.TableName;
-import java.time.LocalDateTime;
+import com.baomidou.mybatisplus.annotation.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 /**
  * <p>
- * 
+ *
  * </p>
  *
- * @author ycshang
+ * @author wg-233
  * @since 2023-11-07
  */
 @Getter
@@ -49,13 +46,16 @@ public class Category {
     @ApiModelProperty("逻辑删除(0-未删除，1-已删除)")
     @TableField("delete_flag")
     @TableLogic
+    @JsonIgnore
     private Integer deleteFlag;
 
     @ApiModelProperty("创建时间")
     @TableField(value = "create_time", fill = FieldFill.INSERT)
+    @JsonIgnore
     private LocalDateTime createTime;
 
     @ApiModelProperty("更新时间")
     @TableField(value = "update_time", fill = FieldFill.INSERT_UPDATE)
+    @JsonIgnore
     private LocalDateTime updateTime;
 }
--- src/main/java/com/soft2242/shop/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/soft2242/shop/service/impl/CategoryServiceImpl.java
@@ -1,21 +1,77 @@
 package com.soft2242.shop.service.impl;
 
-import com.soft2242.shop.entity.Category;
-import com.soft2242.shop.mapper.CategoryMapper;
-import com.soft2242.shop.service.CategoryService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.soft2242.shop.convert.GoodsConvert;
+import com.soft2242.shop.entity.Category;
+import com.soft2242.shop.entity.Goods;
+import com.soft2242.shop.enums.CategoryRecommendEnum;
+import com.soft2242.shop.mapper.CategoryMapper;
+import com.soft2242.shop.mapper.GoodsMapper;
+import com.soft2242.shop.service.CategoryService;
+import com.soft2242.shop.VO.CategoryChildrenGoodsVO;
+import com.soft2242.shop.VO.CategoryVO;
+import com.soft2242.shop.VO.RecommendGoodsVO;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
- * @author ycshang
+ * @author wg-233
  * @since 2023-11-07
  */
 @Service
-
+@AllArgsConstructor
 public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category> implements CategoryService {
 
+    private final GoodsMapper goodsMapper;
+    @Override
+    public List<Category> getIndexCategoryList() {
+        LambdaQueryWrapper<Category> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(Category::getIsRecommend, CategoryRecommendEnum.ALL_RECOMMEND.getValue());
+        wrapper.orderByDesc(Category::getCreateTime);
+        List<Category> list = baseMapper.selectList(wrapper);
+        return list;
+    }
+
+    @Override
+    public List<CategoryVO> getCategoryList() {
+        ArrayList<CategoryVO> list = new ArrayList<>();
+        LambdaQueryWrapper<Category> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(Category::getIsRecommend, CategoryRecommendEnum.ALL_RECOMMEND.getValue()).or();
+        List<Category> categories = baseMapper.selectList(wrapper);
+        LambdaQueryWrapper<Goods> queryWrapper = new LambdaQueryWrapper<>();
+        for (Category category : categories) {
+            CategoryVO categoryVO = new CategoryVO();
+            categoryVO.setId(category.getId());
+            categoryVO.setName(category.getName());
+            categoryVO.setIcon(category.getIcon());
+            wrapper.clear();
+            wrapper.eq(Category::getParentId, category.getId());
+            List<Category> childCategories = baseMapper.selectList(wrapper);
+            ArrayList<CategoryChildrenGoodsVO> categoryChildrenGoodsList = new ArrayList<>();
+            for (Category item : childCategories) {
+                CategoryChildrenGoodsVO childrenGoodsVO = new CategoryChildrenGoodsVO();
+                childrenGoodsVO.setId(item.getId());
+                childrenGoodsVO.setName(item.getName());
+                childrenGoodsVO.setIcon(item.getIcon());
+                childrenGoodsVO.setParentId(category.getId());
+                childrenGoodsVO.setParentName(category.getName());
+                queryWrapper.clear();
+                List<Goods> goodsList = goodsMapper.selectList(queryWrapper.eq(Goods::getCategoryId, item.getId()));
+                List<RecommendGoodsVO> goodsVOList = GoodsConvert.INSTANCE.convertToRecommendGoodsVOList(goodsList);
+                childrenGoodsVO.setGoods(goodsVOList);
+                categoryChildrenGoodsList.add(childrenGoodsVO);
+            }
+            categoryVO.setChildren(categoryChildrenGoodsList);
+            list.add(categoryVO);
+        }
+        return list;
+    }
 }
