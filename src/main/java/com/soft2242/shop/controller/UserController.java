db4e2d111aec38a031d6d4c99ee89aed4d0ccb30
sadwihu <2254443557@qq.com>
2023-11-11 17:07:59 +0800

    添加用户头像

--- src/main/java/com/soft2242/shop/controller/UserController.java
+++ src/main/java/com/soft2242/shop/controller/UserController.java
@@ -13,6 +13,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import static com.soft2242.shop.common.utils.ObtainUserIdUtils.getUserId;
 
@@ -53,6 +54,13 @@ public class UserController {
         userVO.setId(userId);
         UserVO userInfo = userService.editUserInfo(userVO);
         return Result.ok(userInfo);
+    }
+    @Operation(summary = "修改用户头像")
+    @PostMapping("/profile/avatar")
+    private Result<String> editUserAvatar(HttpServletRequest request, MultipartFile file) {
+        Integer userId = getUserId(request);
+        String uploadFileName = userService.editUserAvatar(userId, file);
+        return Result.ok(uploadFileName);
     }
 }
 
