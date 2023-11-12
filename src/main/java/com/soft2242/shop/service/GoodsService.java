5fe1e34c533ea7c8e54ede4ea5bb7250b06ce61e
sadwihu <2254443557@qq.com>
2023-11-12 12:14:32 +0800

    商品详情

--- src/main/java/com/soft2242/shop/service/GoodsService.java
+++ src/main/java/com/soft2242/shop/service/GoodsService.java
@@ -1,16 +1,46 @@
 package com.soft2242.shop.service;
 
+import com.soft2242.shop.common.result.PageResult;
 import com.soft2242.shop.entity.Goods;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.soft2242.shop.query.Query;
+import com.soft2242.shop.query.RecommendByTabGoodsQuery;
+import com.soft2242.shop.VO.GoodsVO;
+import com.soft2242.shop.VO.IndexTabRecommendVO;
+import com.soft2242.shop.VO.RecommendGoodsVO;
+import io.swagger.models.auth.In;
+
 
 /**
  * <p>
  *  服务类
  * </p>
  *
- * @author ycshang
- * @since 2023-11-07
+ *
+ *
  */
 public interface GoodsService extends IService<Goods> {
+    /**
+     * 首页热门推荐 - 根据 tab Id 获取该推荐下的商品列表
+     *
+     * @return
+     */
+    IndexTabRecommendVO getTabRecommendGoodsByTabId(RecommendByTabGoodsQuery query);
 
+
+    /**
+     * 首页推荐 - 猜你喜欢(分页)
+     *
+     * @param query
+     * @return
+     */
+    PageResult<RecommendGoodsVO> getRecommendGoodsByPage(Query query);
+
+    /**
+     * 根据id获取商品信息
+     *
+     * @param id
+     * @return
+     */
+    GoodsVO getGoodsDetail(Integer id);
 }
