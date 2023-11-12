b87bc2e5cde8e08fb21c19b328ee390d4aed7f94
sadwihu <2254443557@qq.com>
2023-11-12 13:03:58 +0800

    首页轮播

--- src/main/java/com/soft2242/shop/service/IndexCarouselService.java
+++ src/main/java/com/soft2242/shop/service/IndexCarouselService.java
@@ -1,16 +1,24 @@
 package com.soft2242.shop.service;
 
-import com.soft2242.shop.entity.IndexCarousel;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.soft2242.shop.entity.IndexCarousel;
+
+import java.util.List;
 
 /**
  * <p>
- *  服务类
+ * 服务类
  * </p>
  *
- * @author ycshang
+ * @author wg-233
  * @since 2023-11-07
  */
 public interface IndexCarouselService extends IService<IndexCarousel> {
-
+    /**
+     * 获取指定分布点的轮播图列表
+     *
+     * @param distributionSite 分布点的标识
+     * @return 分布点对应的轮播图列表
+     */
+    List<IndexCarousel> getList(Integer distributionSite);
 }
--- src/main/java/com/soft2242/shop/service/impl/IndexCarouselServiceImpl.java
+++ src/main/java/com/soft2242/shop/service/impl/IndexCarouselServiceImpl.java
@@ -1,20 +1,33 @@
 package com.soft2242.shop.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.soft2242.shop.entity.IndexCarousel;
 import com.soft2242.shop.mapper.IndexCarouselMapper;
 import com.soft2242.shop.service.IndexCarouselService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
- * @author ycshang
+ * @author wg-233
  * @since 2023-11-07
  */
 @Service
 public class IndexCarouselServiceImpl extends ServiceImpl<IndexCarouselMapper, IndexCarousel> implements IndexCarouselService {
 
+    @Override
+    public List<IndexCarousel> getList(Integer distributionSite) {
+        LambdaQueryWrapper<IndexCarousel> wrapper = new LambdaQueryWrapper<>();
+        if (distributionSite!= null) {
+            wrapper.eq(IndexCarousel::getType, distributionSite);
+        }
+        wrapper.orderByDesc(IndexCarousel::getCreateTime);
+        List<IndexCarousel> list = baseMapper.selectList(wrapper);
+        return list;
+    }
 }
