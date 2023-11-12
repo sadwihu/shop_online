38c2387c92f4c51e495124fd647f1fa9fa3ca2f8
sadwihu <2254443557@qq.com>
2023-11-12 13:01:54 +0800

    热门推荐

--- src/main/java/com/soft2242/shop/service/IndexRecommendService.java
+++ src/main/java/com/soft2242/shop/service/IndexRecommendService.java
@@ -1,7 +1,10 @@
 package com.soft2242.shop.service;
 
+import com.soft2242.shop.VO.IndexRecommendVO;
 import com.soft2242.shop.entity.IndexRecommend;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,4 +16,5 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IndexRecommendService extends IService<IndexRecommend> {
 
+    List<IndexRecommendVO> getList();
 }
