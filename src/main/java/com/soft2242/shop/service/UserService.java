863c92fe9d75065beb50dafe8b246e9daea7ea88
sadwihu <2254443557@qq.com>
2023-11-12 13:02:29 +0800

    修改用户头像

--- src/main/java/com/soft2242/shop/service/UserService.java
+++ src/main/java/com/soft2242/shop/service/UserService.java
@@ -1,39 +1,46 @@
 package com.soft2242.shop.service;
 
+import com.baomidou.mybatisplus.extension.service.IService;
 import com.soft2242.shop.VO.LoginResultVO;
 import com.soft2242.shop.VO.UserVO;
-import com.soft2242.shop.common.exception.ServerException;
 import com.soft2242.shop.entity.User;
-import com.baomidou.mybatisplus.extension.service.IService;
 import com.soft2242.shop.query.UserLoginQuery;
 import com.soft2242.shop.VO.LoginResultVO;
+import com.soft2242.shop.VO.UserVO;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * <p>
- *  服务类
+ * 服务类
  * </p>
  *
- * @author sunyu
- * @since 2023-11-08
+ *
+ * @since 2023-11-07
  */
 public interface UserService extends IService<User> {
+    /**
+     * 登录方法
+     *
+     * @param query 登录查询对象
+     * @return 登录结果对象
+     */
     LoginResultVO login(UserLoginQuery query);
-
-    //    根据用户id 获取用户信息
-//    @paran userId
-//    @return
+    /**
+     * 根据用户ID获取用户信息
+     *
+     * @param userId 用户ID
+     * @return 用户信息
+     */
     User getUserInfo(Integer userId);
 
+    /**
+     * 编辑用户信息
+     *
+     * @param userVO 用户信息实体对象
+     * @return 编辑后的用户信息实体对象
+     */
     UserVO editUserInfo(UserVO userVO);
 
-
-//    @Override
-//    public User getUserInfo(Integer userId) {
-//        User user=baseMapper.selectById(userId);
-//        if (user==null){
-//            throw new ServerException("用户不存在");
-//        }
-//        return user;
-//    }
+    String editUserAvatar(Integer userId, MultipartFile file);
 
 }
